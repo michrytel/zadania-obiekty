b8afcc52153edba2c1226576aad88f90e677c039
michrytel <76482065+michrytel@users.noreply.github.com>
2021-02-05 17:31:58 +0100

    Update app.js

---
+++ 1-zadania/js/app.js
@@ -0,0 +1,28 @@
+function prod(name, price, weight) {
+        this.name = name
+        this.price = price
+        this.weight = weight
+}
+
+const prod1 = new prod("prod1", 10, 20)
+const prod2 = new prod("prod2", 130, 210)
+
+console.log(prod1);
+console.log(prod2);
+
+console.log(`Produkt numer jeden to: ${prod1.name}`);
+console.log(`Produkt numer jeden to: ${prod2.name}`);
+console.log(`Produkty kosztuja razem: ` + (prod1.price + prod2.price));
+console.log(`Produkty waza razem: ` + (prod1.weight + prod2.weight));
+
+let currentUser = {
+    name: "Michal",
+    surname: "Michalowski",
+    email: "michryt@",
+    showAll() {
+            console.log(`imie to ${this.name}`)
+
+    }
+}
+
+currentUser.showAll()
